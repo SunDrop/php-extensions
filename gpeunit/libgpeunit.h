de2b0e5b480001f796278c3be89d15a64219949f
Slava Konashkov <s.konashkov@unit.com.ua>
2018-01-24 15:20:50 +0200

    CMAPI-4 Создание extention Анализатора for PHP v2.0

--- gpeunit/libgpeunit.h
+++ gpeunit/libgpeunit.h
@@ -10,15 +10,25 @@
 extern "C" {
 #endif
 
-GPE_UNIT_IMPORT int AnalyseGPE(const void* image, int imageSize, int extrnWhite, int blackBorder, int internWhite, int GPESize,
-                          int seed1, int seed2, int seed3, int seed4, int localShiftsCount, int localShiftSize,
-                          float RefPointsCountPercent, float NoiceCountPercent, const void* rsMemory, int rsMemorySize,
-                          void** resultMemory, int* resultMemorySize);
+struct AnalyseGPEResult_t{
+    int status;
+    char *GPEData;
+    double score;
+    double scoreAdv;
+    uint32_t localShifts;
+} __attribute__((aligned(1)));
 
-GPE_UNIT_IMPORT int GenerateGPE(int qrUnitSize, const char* qrData, int extrnWhite, int blackBorder, int internWhite, int GPESize,
-                           int seed1, int seed2, int seed3, int seed4, const char* gpeData, int localShiftsCount, int localShiftSize,
-                           float RefPointsCountPercent, float NoiceCountPercent, int DPI,
-                           void** gpeMemory, int* gpememorySize, void** rsMemory, int* rsMemorySize);
+GPE_UNIT_IMPORT int
+AnalyseGPE(const void *image, int imageSize, int extrnWhite, int blackBorder, int internWhite, int GPESize,
+           int seed1, int seed2, int seed3, int seed4, int localShiftsCount, int localShiftSize,
+           float RefPointsCountPercent, float NoiceCountPercent, const void *rsMemory, int rsMemorySize,
+           void **resultMemory, int *resultMemorySize);
+
+GPE_UNIT_IMPORT int
+GenerateGPE(int qrUnitSize, const char *qrData, int extrnWhite, int blackBorder, int internWhite, int GPESize,
+            int seed1, int seed2, int seed3, int seed4, const char *gpeData, int localShiftsCount, int localShiftSize,
+            float RefPointsCountPercent, float NoiceCountPercent, int DPI,
+            void **gpeMemory, int *gpememorySize, void **rsMemory, int *rsMemorySize);
 
 #ifdef __cplusplus
 }
--- gpeunit/php_gpeunit.h
+++ gpeunit/php_gpeunit.h
@@ -1,30 +1,15 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2016 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id$ */
-
 #ifndef PHP_GPEUNIT_H
 #define PHP_GPEUNIT_H
 
 extern zend_module_entry gpeunit_module_entry;
 #define phpext_gpeunit_ptr &gpeunit_module_entry
 
+#define PHP_GPEUNIT_EXTNAME "gpeunit" /* Replace with version number for your extension */
 #define PHP_GPEUNIT_VERSION "2.0.0" /* Replace with version number for your extension */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
 
 #ifdef PHP_WIN32
 #	define PHP_GPEUNIT_API __declspec(dllexport)
@@ -38,26 +23,6 @@ extern zend_module_entry gpeunit_module_entry;
 #include "TSRM.h"
 #endif
 
-/* 
-  	Declare any global variables you may need between the BEGIN
-	and END macros here:     
-
-ZEND_BEGIN_MODULE_GLOBALS(gpeunit)
-	long  global_value;
-	char *global_string;
-ZEND_END_MODULE_GLOBALS(gpeunit)
-*/
-
-/* In every utility function you add that needs to use variables 
-   in php_gpeunit_globals, call TSRMLS_FETCH(); after declaring other 
-   variables used by that function, or better yet, pass in TSRMLS_CC
-   after the last function argument and declare your utility function
-   with TSRMLS_DC after the last declared argument.  Always refer to
-   the globals in your function as GPEUNIT_G(variable).  You are 
-   encouraged to rename these macros something shorter, see
-   examples in any other php module directory.
-*/
-
 #ifdef ZTS
 #define GPEUNIT_G(v) TSRMG(gpeunit_globals_id, zend_gpeunit_globals *, v)
 #else
@@ -65,13 +30,3 @@ ZEND_END_MODULE_GLOBALS(gpeunit)
 #endif
 
 #endif	/* PHP_GPEUNIT_H */
-
-
-/*
- * Local variables:
- * tab-width: 4
- * c-basic-offset: 4
- * End:
- * vim600: noet sw=4 ts=4 fdm=marker
- * vim<600: noet sw=4 ts=4
- */
